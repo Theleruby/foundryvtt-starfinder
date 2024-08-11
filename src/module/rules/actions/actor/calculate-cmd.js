306fd29c681072da3fb9ae389e476de1741a11af
Ruby Dennington <theleruby@gmail.com>
2024-08-11 12:06:17 +0100

    Reduce combat maneuver difficulty to KAC+4

--- src/module/rules/actions/actor/calculate-cmd.js
+++ src/module/rules/actions/actor/calculate-cmd.js
@@ -3,7 +3,7 @@ export default function(engine) {
         const cmd = fact.data.attributes.cmd;
         const kac = fact.data.attributes.kac;
 
-        cmd.value = 8 + kac.value;
+        cmd.value = 4 + kac.value;
         cmd.tooltip.push(game.i18n.localize("SFRPG.CMDBaseTooltip"));
         cmd.tooltip.push(game.i18n.format("SFRPG.CMDKACModTooltip", { kac: kac.value.signedString() }));
 
